23f870d782f6ddff394630be8ca435eabad3e53c
JensV <jens.vansteenvoort@gmail.com>
2020-12-12 14:17:41 +0100

    test

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+Info
+"""
+
+
+__author__ = "Jens Vansteenvoort"
+__email__ = "jens.vansteenvoort@student.kdg.be"
+__status__ = "Development"
+
+
+from gpiozero import LED
+from time import sleep
+
+led = LED(18)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Info
+"""
+
+
+import
+from gpiozero import LED
+
+__author__ = "Jens Vansteenvoort"
+__email__ = "jens.vansteenvoort@student.kdg.be"
+__status__ = "Development"
+
+
+def main()
+
+
+if __name__ == '__main__':    #code to execute if called from command-line
+    main()
