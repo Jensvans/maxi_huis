46f9ecfe28101b0b40c8d0d47512c3fce2044cca
JensV <jens.vansteenvoort@gmail.com>
2020-12-15 18:51:47 +0100

    test

--- button test.py
+++ button test.py
@@ -4,6 +4,7 @@ Info 1 druknop die 2 led's bediend
 """
 
 from signal import pause
+from time import sleep
 from gpiozero import LED, Button
 
 
@@ -16,8 +17,23 @@ led1 = LED(17)
 led2 = LED(22)
 button1 = Button(18)
 
+
+def go_blink():
+    global blink_on
+
+    if blink_on:
+        led1.off()
+        led2.off()
+    else:
+        led1.blink(0.5, 0.5)
+        sleep(0.5)
+        led2.blink(0.5, 0,5)
+
+    blink_on = not blink_on
+
 try:
-    led1.source = button1.value
+    button1.when_pressed = go_blink
+
     pause()
 finally:
     pass
--- main.py
+++ main.py
@@ -16,9 +16,3 @@ led1 = LED(17)
 led2 = LED(22)
 button = Button(18)
 
-def toggle_led():
-    if button.is_pressed():
-        led1.toggle()
-
-if __name__== '__main__':
-    toggle_led()
