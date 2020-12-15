0b72dadd95776051fef9d07f61364c1fa0b90a5c
JensV <jens.vansteenvoort@gmail.com>
2020-12-15 18:31:24 +0100

    test

---
+++ button test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+Info 1 druknop die 2 led's bediend
+"""
+
+
+from gpiozero import LED, Button
+from time import  sleep
+
+__author__ = "Jens Vansteenvoort"
+__email__ = "jens.vansteenvoort@student.kdg.be"
+__status__ = "Development"
+
+
+led1 = LED(17)
+led2 = LED(22)
+button1 = Button(18)
+
+try:
+    led1.source = button1.value
+    pause()
+finally:
+    pass
+
+if __name__== '__main__':
+    toggle_led()
