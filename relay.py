fb2e524ff2a055c24f8b317784a64ab7056da0a7
JensV <jens.vansteenvoort@gmail.com>
2020-12-22 16:54:18 +0100

    test

--- relay.py
+++ relay.py
@@ -3,33 +3,34 @@
 Info: tesing how a relay functions in gpiozore
 """
 
-from signal import pause
-from time import sleep
-from gpiozero import LED, Button, OutputDevice
+
+import time
+import gpiozero
 
 
 __author__ = "Jens Vansteenvoort"
 __email__ = "jens.vansteenvoort@student.kdg.be"
 __status__ = "Development"
 
-
-import sys
-import time
-import gpiozero
-
-relay = gpiozero.OutputDevice(23, active_high=False, initial_value=False)
+relay_pin1 = 23
+relay_pin2 = 24
+relay1 = gpiozero.OutputDevice(relay_pin1, active_high=False, initial_value=False)
+relay2 = gpiozero.OutputDevice(relay_pin2, active_high=False, initial_value=False)
 
 def set_relay(status):
     if status:
         print("Setting relay: ON")
-        relay.on()
+        relay1.on()
+        relay2.on()
     else:
         print("Setting relay: OFF")
-        relay.off()
+        relay1.off()
+        relay2.off()
 
 def toggle_relay():
     print("toggling relay")
-    relay.toggle()
+    relay1.toggle()
+    relay2.toggle()
 
 def main_loop():
     while 1:
@@ -46,4 +47,3 @@ if __name__ == "__main__":
         set_relay(False)
         print("\nExiting application\n")
         # exit the application
-        sys.exit(0)
